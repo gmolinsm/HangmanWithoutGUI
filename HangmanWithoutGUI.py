54b6346ebb06790d7d32ae7f3105ccf67c0f11d5
Guillermo Molins <guimolins@gmail.com>
2018-11-18 21:40:32 +0100

    UI Added but buttons wont work

--- HangmanWithoutGUI.py
+++ HangmanWithoutGUI.py
@@ -1,64 +1,125 @@
 import random
+from tkinter import *
+from PIL import Image, ImageTk
 
-fd = open("Words", "r")
-random = random.randint(0, 9)
-temp_solution = ""
-solution = list()
-progress = list()
-view_progress = list()
-lives = 6
-wrong_letters = ""
 
-index = 0
-for line in fd:
-    if index == random:
-        temp_solution = line
-    index += 1
 
-for i in range(len(temp_solution)-1):
-    if temp_solution[i] == " ":
-        view_progress.append(" ")
-    else:
-        view_progress.append("_")
-        progress.append("")
-        solution.append(temp_solution[i])
 
-print(view_progress)
-print()
-while True:
-    letter = input("Enter a letter: ")
-    index_with_space = 0
-    index = 0
-    correct = False
-    for character in solution:
-        if temp_solution[index_with_space] == " ":
+class UI:
+    def __init__(self, window):
+        fd = open("Words", "r")
+        self.random = random.randint(0, 9)
+        self.temp_solution = ""
+        self.solution = list()
+        self.progress = list()
+        self.view_progress = list()
+        
+        self.alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        self.stages = ["assets/S1.png", "assets/S2.png", "assets/S3.png", "assets/S4.png",
+                  "assets/S5.png", "assets/S6.png", "assets/S8.png", "assets/S10.png"]
+        self.lives = 8
+        self.wrong_letters = ""
+
+        self.window = window
+        self.idx = 0
+        window.title("Hangman")
+        title_font = ("Times New Roman", 40)
+        self.solution_font = ("Times New Roman", 30)
+        alphabet_font = ("Times New Roman", 10)
+        buttons_font = ("Times New Roman", 20)
+
+        # Hangman viewport
+        self.stage_list = []
+        self.count = 0
+        for i in range(8):
+            self.stage_list.append(ImageTk.PhotoImage(Image.open(self.stages[i])))
+        self.viewport = Label(master=window, image=self.stage_list[self.count])
+        self.viewport.image = self.stage_list[self.count]
+        self.viewport.grid(row=2, column=0)
+
+        # Text
+        self.lbl1 = Label(master=window, text="Hangman", font=title_font)
+        self.lbl1.grid(row=0, column=1)
+
+        self.lbl2 = Label(master=window, text=self.view_progress, font=self.solution_font)
+        self.lbl2.grid(row=3, column=1)
+
+        self.lbl3 = Label(master=window, text=self.wrong_letters + " " + str(self.lives-1), font=self.solution_font)
+        self.lbl3.grid(row=2, column=1)
+
+        # Buttons
+        self.buttons = []
+        self.gridpos = []
+        count = 2
+        for i in range(0, len(self.alphabet)):
+            self.buttons.append(Button(master=window, text=self.alphabet[i], width=2,
+                                       font=alphabet_font, command=self.check)) # ????????????????????????
+            self.gridpos.append(self.buttons[i].grid(row=1, column=count))
+            count += 1
+
+        close = Button(master=window, text="Exit", width=6, font=buttons_font, command=self.close)
+        close.grid(row=1, column=0)
+
+        index = 0
+        for line in fd:
+            if index == random:
+                self.temp_solution = line
+            index += 1
+
+        for i in range(len(self.temp_solution) - 1):
+            if self.temp_solution[i] == " ":
+                self.view_progress.append(" ")
+            else:
+                self.view_progress.append("_")
+                self.progress.append("")
+                self.solution.append(self.temp_solution[i])
+
+    def close(self):
+        exit(0)
+
+    def check(self):
+        letter = "a"
+        index_with_space = 0
+        index = 0
+        correct = False
+        for character in self.solution:
+            if self.temp_solution[index_with_space] == " ":
+                index_with_space += 1
+            if character == letter:
+                self.view_progress[index_with_space] = letter
+                self.progress[index] = letter
+                correct = True
             index_with_space += 1
-        if character == letter:
-            view_progress[index_with_space] = letter
-            progress[index] = letter
-            correct = True
-        index_with_space += 1
-        index += 1
-    if correct:
-        print()
-        print(view_progress)
-        print()
-        if progress == solution:
-            print("Congratulations, you WON!")
-            break
-    else:
-        lives -= 1
-        if lives == 0:
+            index += 1
+        if correct:
             print()
-            print("Tough luck, you reached the maximum failed attempts... GAME OVER")
+            print(self.view_progress)
             print()
-            print("The solution was:", solution)
-            break
+            if self.progress == self.solution:
+                self.close()
+                print("Congratulations, you WON!")
         else:
-            wrong_letters += letter + " "
-            print()
-            print("Incorrect! You have", lives, "failed attempts left...")
-            print("Wrong letters:", wrong_letters)
-            print()
-            print(view_progress)
-            print()
+            self.lives -= 1
+            self.count += 1
+            if self.lives == 0:
+                print()
+                print("Tough luck, you reached the maximum failed attempts... GAME OVER")
+                print()
+                print("The self.solution was:", self.solution)
+                window.after(50000, self.close())
+            else:
+                self.wrong_letters += letter + " "
+                print()
+                print("Incorrect! You have", self.lives-1, "failed attempts left...")
+                print("Wrong letters:", self.wrong_letters)
+                print()
+                print(self.view_progress)
+                print()
+        self.lbl2["text"] = self.view_progress
+        self.lbl3["text"] = str(self.wrong_letters) + str(self.lives-1)
+        self.viewport["image"] = self.stage_list[self.count]
+
+window = Tk()
+front = UI(window)
+window.mainloop()
