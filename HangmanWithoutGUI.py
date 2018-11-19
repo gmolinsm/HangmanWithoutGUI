6f18801dd88122b449bab011f0cb39c04f685ec1
Guillermo <guimolins@hotmail.com>
2018-11-19 13:53:47 +0100

    Fixed EVERYTHING

--- HangmanWithoutGUI.py
+++ HangmanWithoutGUI.py
@@ -1,7 +1,7 @@
 import random
 from tkinter import *
 from PIL import Image, ImageTk
-
+from tkinter import messagebox
 
 class UI:
     def __init__(self, window):
@@ -26,28 +26,28 @@ class UI:
                 self.view_progress.append(" ")
             else:
                 self.view_progress.append("_")
-                self.progress.append("")
+                self.progress.append(" ")
                 self.solution.append(self.temp_solution[i])
 
         self.alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
                     "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
         self.stages = ["assets/S1.png", "assets/S2.png", "assets/S3.png", "assets/S4.png",
-                  "assets/S5.png", "assets/S6.png", "assets/S8.png", "assets/S10.png"]
-        self.lives = 8
-        self.wrong_letters = ""
+                  "assets/S5.png", "assets/S6.png", "assets/S8.png", "assets/S10.png", "assets/S11.png"]
+        self.lives = 7
+        self.wrong_letters = "Wrong letters: "
 
         self.window = window
         self.idx = 0
         window.title("Hangman")
         self.title_font = ("Times New Roman", 40)
-        self.solution_font = ("Times New Roman", 20)
-        self.alphabet_font = ("Times New Roman", 10)
+        self.solution_font = ("Times New Roman", 15)
+        self.alphabet_font = ("Times New Roman", 12)
         self.buttons_font = ("Times New Roman", 20)
 
         # Hangman viewport
         self.stage_list = []
         self.count = 0
-        for i in range(8):
+        for i in range(9):
             self.stage_list.append(ImageTk.PhotoImage(Image.open(self.stages[i])))
         self.viewport = Label(master=window, image=self.stage_list[self.count])
         self.viewport.image = self.stage_list[self.count]
@@ -57,36 +57,64 @@ class UI:
         self.lbl1 = Label(master=window, text="Hangman", font=self.title_font)
         self.lbl1.grid(row=0, column=1)
 
-        self.lbl2 = Label(master=window, text=str(self.view_progress), font=self.solution_font)
-        self.lbl2.grid(row=3, column=1)
+        self.lbl2 = Label(master=window, text=self.print_progress(self.view_progress), font=self.solution_font)
+        self.lbl2.grid(row=4, column=1)
 
-        self.lbl3 = Label(master=window, text=str(self.wrong_letters) + " " + str(self.lives-1), font=self.solution_font)
-        self.lbl3.grid(row=2, column=1)
+        self.lbl4 = Label(master=window, text="Lives: " + str(self.lives), font=self.solution_font)
+        self.lbl4.grid(row=1, column=2)
 
         # Buttons
         self.buttons = []
         self.gridpos = []
-        column = 2
-        for i in range(0, len(self.alphabet)):
+
+        column = 3
+        for i in range(len(self.alphabet)):
             self.buttons.append(Button(master=window, text=self.alphabet[i], width=2,
-                                       font=self.alphabet_font, command=self.check)) # ????????????????????????
+                                       font=self.alphabet_font, command=lambda i=i: self.check(i)))
             self.gridpos.append(self.buttons[i].grid(row=1, column=column))
             column += 1
 
+        self.lbl3 = Label(master=window, text=str(self.wrong_letters), font=self.solution_font)
+        self.lbl3.grid(row=3, column=0)
+
         close = Button(master=window, text="Exit", width=6, font=self.buttons_font, command=self.close)
         close.grid(row=1, column=0)
+
+    def print_progress(self, list):
+        string = ""
+        for i in range(len(list)):
+            string += " " + list[i]
+        return string
+
+    def print_solution(self, list):
+        string = ""
+        for i in range(len(list)):
+            string += list[i]
+        return string
 
     def close(self):
         exit(0)
 
     # This will refresh data on screen
     def update_view(self):
-        self.lbl2["text"] = str(self.view_progress)
-        self.lbl3["text"] = str(self.wrong_letters) + str(self.lives - 1)
-        self.viewport["image"] = self.stage_list[self.count]
+        if self.lives > -1:
+            self.lbl2["text"] = self.print_progress(self.view_progress)
+            self.lbl3["text"] = str(self.wrong_letters)
+            self.lbl4["text"] = "Lives: " + str(self.lives)
+            self.viewport["image"] = self.stage_list[self.count]
 
-    def check(self):
-        letter = "a"
+        if self.lives == 0:
+            self.lbl2["text"] = self.print_solution(self.temp_solution)
+
+        if self.progress == self.solution and self.lives > 0:
+            self.viewport["image"] = self.stage_list[8]
+            self.lives = -1
+
+    def reset(self):
+        self.lives = 7
+
+    def check(self, i):
+        letter = self.alphabet[i]
         correct = False
         index = 0
         index_with_space = 0
@@ -99,30 +127,11 @@ class UI:
                 correct = True
             index_with_space += 1
             index += 1
-        if correct:
-            print()
-            print(self.view_progress)
-            print()
-            if self.progress == self.solution:
-                self.close()
-                print("Congratulations, you WON!")
-        else:
+        if not correct:
             self.lives -= 1
             self.count += 1
-            if self.lives == 0:
-                print()
-                print("Tough luck, you reached the maximum failed attempts... GAME OVER")
-                print()
-                print("The self.solution was:", self.solution)
-                window.after(50000, self.close())
-            else:
+            if self.lives != 0:
                 self.wrong_letters += letter + " "
-                print()
-                print("Incorrect! You have", self.lives-1, "failed attempts left...")
-                print("Wrong letters:", self.wrong_letters)
-                print()
-                print(str(self.view_progress))
-                print()
         self.update_view()
 
 
